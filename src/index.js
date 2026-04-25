60acb1ee042a40d77d3c4ee49c1f3b5339d7ae5e
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2026-04-26 01:50:04 +0200

    Remove basename /holidaze

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/holidaze">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
